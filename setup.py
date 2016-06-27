5781ac6d2374798582fdb9a64f789a4bdada9455
Pablo Romano <pgromano@users.noreply.github.com>
2016-06-27 10:28:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name = 'LE4PD',
     version = '0.1',
     description = 'Python API for Protein Dynamics using the Langevin Formalism (LE4PD)',
-    #url = 'https://github.com/pgromano/LE4PD',
+    url = 'https://github.com/GuenzaLan/LE4PD',
     packages = ['LE4PD'],
     install_requires=[
         'numpy',

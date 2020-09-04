a88e2abe8793ded0c900318d18ea6f2ed4fd31e5
Eric Beyerle <ebeyerle@uoregon.edu>
2020-09-04 15:28:29 -0700

    Add model save/load functionality via pickling

---
+++ LE4PD/util/model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import subprocess
+import os
+import pickle
+
+#Right now, I am just going to use Python's pickle library to save and load
+#LE4PD models. There are fancier ways to save these things (see, e.g., PyEMMA),
+#but I'm not going to go that far at the moment.
+
+def save(self, filename = "model.p"):
+	if os.path.exists(filename):
+		print("Overwriting " + filename + ". I do hope that is okay.")
+	pickle.dump(self, open(filename, "wb"))
+
+def load(self, filename = "model.p"):
+	try:
+		pickle.dump(self, open(filename, "wb"))
+	except FileNotFoundError:
+		print(filename + " does not exist. Is the correct path specified?")

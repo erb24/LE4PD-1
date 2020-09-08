a9b959ec414c86e3905e03fcdb8853bbae4b5ddb
Eric Beyerle <ebeyerle@uoregon.edu>
2020-09-07 20:57:30 -0700

    Change __init__.py

--- LE4PD/__init__.py
+++ LE4PD/__init__.py
@@ -1,4 +1,4 @@
 from .molecule import protein
-from .dynamics import ensemble, simulation
-from .util import _ensemble_dynamics, _simulation_dynamics
-from .util import analyze_ensemble, analyze_simulation, matrix, prepare
+#from .dynamics import ensemble, simulation
+#from .util import _ensemble_dynamics, _simulation_dynamics
+from .util import prepare
--- LE4PD/util/__init__.py
+++ LE4PD/util/__init__.py
@@ -1,2 +1,2 @@
-from . import analyze_ensemble, analyze_simulation, matrix, prepare, m1, model
-from . import _ensemble_dynamics, _simulation_dynamics
+from . import prepare, m1, model
+#from . import _ensemble_dynamics, _simulation_dynamics

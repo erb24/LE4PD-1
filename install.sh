ec52f2314ccd3f4f84cd3ec45a15acc183340c60
Eric Beyerle <ebeyerle@uoregon.edu>
2020-09-07 20:48:04 -0700

    Clean up

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ cd LE4PD/util/
 rm -rfv __pycache__
 rm -rfv *.so
 
-f2py -m _ensemble_dynamics --fcompiler=gfortran --link-llapack -c _ensemble_dynamics.f95
-f2py -m _simulation_dynamics --fcompiler=gfortran --link-llapack -c _simulation_dynamics.f95
+#f2py -m _ensemble_dynamics --fcompiler=gfortran --link-llapack -c _ensemble_dynamics.f95
+#f2py -m _simulation_dynamics --fcompiler=gfortran --link-llapack -c _simulation_dynamics.f95
 
 f2py --quiet -m _m1int --fcompiler=gfortran -c p2_writeCAint.f
 f2py --quiet -m _m1rot --fcompiler=gfortran -c p2_writeCArot.f
